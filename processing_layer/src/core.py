0736ff887fa2faab3b68ba9d0dc9c03ceb8c0e8c
Jay <jay.young@gmail.com>
2018-05-11 11:08:44 +0100

    even more layers!

---
+++ processing_layer/src/core.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import roslib
+import rospy
+import sensor_msgs
+import geometry_msgs
+import tf
+import uuid
+import json
+
+class ProcessingLayer:
+    def __init__(self):
+        rospy.init_node('molar_processing_layer', anonymous = False)
+        rospy.loginfo("* Processing Layer Starting Up")
+        self.service = rospy.Service('/molar/process_scene', MolarProcessScene, self.process_scene_cb)
+        rospy.loginfo("* Processing Layer Online")
+
+    def process_scene(self,scene):
+
+        #
+        #   send  to filter layer
+        #   do stuff with the result
+        #
+
+        pass
+
+    def process_scene_cb(self,req):
+        return MolarProcessSceneResponse(self.process_scene(req.input))
+
+
+
+
+if __name__ == '__main__':
+    l = ProcessingLayer()
+    #l.read_scene("what")
