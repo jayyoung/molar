9b3f37c1052ab029b35343452edc80fac7ef320b
Jay <jay.young@gmail.com>
2018-05-14 14:49:39 +0100

    added fetch test

---
+++ molar_tools/scripts/fetch_perc_grab.py
@@ -0,0 +1,35 @@
+import roslib
+import rospy
+import sensor_msgs
+import geometry_msgs
+import tf2_ros
+import tf, tf2_msgs.msg
+import os
+import json
+import pickle
+
+
+
+if __name__ == '__main__':
+    rospy.init_node('molar_sanity_check_server', anonymous = False)
+    rospy.loginfo("* Perception grabber server starting up")
+
+    rospy.loginfo("pcd")
+    point_cloud = rospy.wait_for_message("/head_camera/depth/image",Image)
+    rospy.loginfo("rgb")
+
+    rgb_img = rospy.wait_for_message("/head_camera/rgb/image",Image)
+    rospy.loginfo("depth")
+
+    depth_img = rospy.wait_for_message("/head_camera/rgb/depth",Image)
+    rospy.loginfo("cinfo")
+
+    camera_info = rospy.wait_for_message("/head_camera/rgb/camera_info",CameraInfo)
+    rospy.loginfo("robotpose")
+
+
+    robot_pose = rospy.wait_for_message("/amcl_pose",PoseWithCovarianceStamped)
+    rospy.loginfo("tf")
+
+
+    tf = rospy.wait_for_message("/tf",TFMessage)
